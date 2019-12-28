3466013c1c9c2bfc803a3bbb98afe95f21af19b1
unknown <helene.carlier.gubler@gmail.com>
2019-12-28 19:35:20 +0100

    Boostrap 1er composant

--- my_app/src/index.js
+++ my_app/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import {Col, Container, Row} from "react-bootstrap";
 import DayTemperature from "../src/javascript/components/DayTemperature";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './stylesheets/App.css';
+import './stylesheets/index.css';
 
 class Index extends React.Component{
 	
